d36a319b7191d4d664a23ca3872a6ceb81b3d9a3
martins-ggm <guilherme.ggm@outlook.com>
2025-08-27 13:14:26 -0300

    teste

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,11 +6,11 @@ const Header: React.FC = () => {
     <header className={styles.header}>
       <h1 className={styles.logoField}>&quot;Cabeçalho&quot;</h1>
       <div className={styles.searchField}>
-        <input type="text" className={styles.searchBar}/>
+        <input type="text" className={styles.searchBar} />
       </div>
       <div className={styles.headerButtons}>
-        <h3>entrar</h3>
-        <h3>criar conta</h3>
+        <button>entrar</button> 
+        <button>criar conta</button>
       </div>
     </header>
   );
