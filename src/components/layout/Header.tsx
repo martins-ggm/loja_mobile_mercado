6007987d128c6b19dbbc2c59f8ec0caa584102e6
martins-ggm <guilherme.ggm@outlook.com>
2025-08-27 01:36:27 -0300

    importação do global.styles e implementação de estrutura do header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import styles from './styles/Header.module.css';
+import React from "react";
+import styles from "./styles/Header.module.css";
 
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-     <h1>&quot;Cabeçalho&quot;</h1>
+      <h1 className={styles.logoField}>&quot;Cabeçalho&quot;</h1>
+      <div className={styles.searchField}>
+        <h1>Encontre os produtos que busca:</h1>
+      </div>
     </header>
   );
 };
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../layout/Header';
 import Main from '../layout/Main';
 import styles from './styles/Home.module.css'; // Import the CSS module
-
+import '/src/app/globals.css';
 
 const Home: React.FC = () => {
     return (
