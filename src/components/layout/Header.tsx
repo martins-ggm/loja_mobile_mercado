52be2bc545cc90def6dea481218cfcafdfdc0ab7
martins-ggm <guilherme.ggm@outlook.com>
2025-08-27 23:58:36 -0300

    tamanho da searchbar definido, inserção de logo padrão

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,13 +4,15 @@ import styles from "./styles/Header.module.css";
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-      <h1 className={styles.logoField}>&quot;Cabeçalho&quot;</h1>
+      <div className={styles.logoField}>
+        <img src="/logo_mobile.png" alt="logo" className={styles.logo} />
+      </div>
       <div className={styles.searchField}>
         <label htmlFor="search">Encontre os produtos que busca:</label>
         <input name="search" type="text" className={styles.searchBar} />
       </div>
       <div className={styles.headerButtons}>
-        <button className={styles.singinButton}>entrar</button> 
+        <button className={styles.singinButton}>entrar</button>
         <button className={styles.singupButton}>criar conta</button>
       </div>
     </header>
